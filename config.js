c259ffad2e08ffed629a581c40e29c9b272659d0
practice-harpreetsd99 <67538183+practice-harpreetsd99@users.noreply.github.com>
2020-06-28 14:03:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBNuyR1kYlUE47dRdvSwH_ktun0aJdmnIw",
+    authDomain: "codellion-project.firebaseapp.com",
+    databaseURL: "https://codellion-project.firebaseio.com",
+    projectId: "codellion-project",
+    storageBucket: "codellion-project.appspot.com",
+    messagingSenderId: "115691137424",
+    appId: "1:115691137424:web:6512ab76b92862f9302b15",
+    measurementId: "G-QDVW8S0KLN"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
